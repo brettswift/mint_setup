7cacae06c0d34a6f49446da7cd57b9b18f4b8d4f
Brett Swift <brettswift@gmail.com>
2019-06-17 06:20:22 -0600

    Add comment for running on docker if apt-add-repository isn't installed

--- packages.sh
+++ packages.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# if on docker, or minimal ubuntu, you might need to:
+#apt-get install software-properties-common
+####
+
 add-apt-repository ppa:cpick/hub -y
 
 apt-get update
